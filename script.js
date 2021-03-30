5fa1ae4dfeb8d4291dcdcc06a0b1d9898e45bf70
julieli1 <missvckise@gmail.com>
2021-03-30 00:04:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// i connected it to a button because I wanted to look at the website before getting spammed by 'guess my age'
+document.getElementById('play').addEventListener('click', play)
+
+// sets my age to 15, user guess to 0
+let age = 15
+let guess = 0
+
+function play () {
+  while (guess != age) {
+    guess = prompt('guess my age')
+    if (age == guess) {
+      alert('congrats, you guessed right')
+    } else if (age > guess) {
+      alert('try higher')
+    } else if (age < guess) {
+      alert('try lower')
+    } else {
+      alert('please enter a valid number')
+    }
+  }
+}
